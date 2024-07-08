db27a13fb0389cfef11bb30bcab7c3ab68b7e903
ThanakornSaokum <thanakorn01350@gmail.com>
2024-07-08 22:56:53 +0700

    finish

--- q1.ts
+++ q1.ts
@@ -1,9 +1,19 @@
 // const axios = require("axios");
+// 660610761 ธนกร เสาคำ
 import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getUser = async (userId: number) => {
   /* Your code here */
+  try{
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const user = response.data;
+    return `${user.name}`;
+
+  } 
+  catch(error){
+    return 'INVALID USER ID';  
+  }
 };
 
 //test case
--- q2.ts
+++ q2.ts
@@ -1,9 +1,29 @@
 // const axios = require("axios");
+// 660610761 ธนกร เสาคำ
 import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
   /* Your code here */
+  try{
+    const resTodo = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    const todo = resTodo.data;
+    const userId = todo.userId;
+
+    const resUser = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const user = resUser.data;
+
+    const new_todo = {
+      owner: user.name,
+      title: todo.title,
+      completed: todo.completed
+    };
+    return new_todo;
+
+  }
+  catch(error){
+    return 'INVALID TODO ID';
+  }
 };
 
 //test case
